d4278b904f3dc0224ac135313cf536312e915399
Józef Sokołowski <j.k.sokolowski@gmail.com>
2016-05-25 16:15:21 +0200

    Change code style to "standard"

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # dustman
 
+[![js-standard-style](https://img.shields.io/badge/code%20style-standard-brightgreen.svg)](http://standardjs.com/)
+
 Firefox alternative for Tab Wrangler. Automatically closes old unused tabs.
--- data/panel.js
+++ data/panel.js
@@ -1,52 +1,55 @@
-const links = {};
+/* global self */
+'use strict'
+
+const links = {}
 
 function removeLink ({ url }) {
-    if (links[url]) {
-        links[url].remove();
-        delete links[url];
-        self.port.emit('link-removed', { url });
-    }
+  if (links[url]) {
+    links[url].remove()
+    delete links[url]
+    self.port.emit('link-removed', { url })
+  }
 }
 
 function addLink ({ url, title, favicon }) {
-    if (url === 'about:blank' || url === 'about:newtab') {
-        return;
-    }
+  if (url === 'about:blank' || url === 'about:newtab') {
+    return
+  }
 
-    removeLink({ url });
+  removeLink({ url })
 
-    const section = document.querySelector('section.recently-closed');
-    const link = document.createElement('a');
-    const text = document.createTextNode(title);
-    const icon = document.createElement('img');
+  const section = document.querySelector('section.recently-closed')
+  const link = document.createElement('a')
+  const text = document.createTextNode(title)
+  const icon = document.createElement('img')
 
-    icon.src = favicon || 'default-favicon.png';
+  icon.src = favicon || 'default-favicon.png'
 
-    link.href = url;
-    link.target = '_blank';
-    link.appendChild(icon);
-    link.appendChild(text);
-    link.onclick = () => removeLink({ url });
+  link.href = url
+  link.target = '_blank'
+  link.appendChild(icon)
+  link.appendChild(text)
+  link.onclick = () => removeLink({ url })
 
-    links[url] = link;
-    section.insertBefore(link, section.firstChild);
-    self.port.emit('link-added', { url, title, favicon });
+  links[url] = link
+  section.insertBefore(link, section.firstChild)
+  self.port.emit('link-added', { url, title, favicon })
 }
 
 function togglePause () {
-    const classList = document.querySelector('.button').classList;
+  const classList = document.querySelector('.button').classList
 
-    if (classList.contains('paused')) {
-        classList.remove('paused');
-        self.port.emit('resume-clicked');
-    } else {
-        classList.add('paused');
-        self.port.emit('pause-clicked');
-    }
+  if (classList.contains('paused')) {
+    classList.remove('paused')
+    self.port.emit('resume-clicked')
+  } else {
+    classList.add('paused')
+    self.port.emit('pause-clicked')
+  }
 }
 
 // Handle events
 
-self.port.on('add-link', addLink);
-self.port.on('remove-link', removeLink);
-document.querySelector('.button').onclick = togglePause;
+self.port.on('add-link', addLink)
+self.port.on('remove-link', removeLink)
+document.querySelector('.button').onclick = togglePause
